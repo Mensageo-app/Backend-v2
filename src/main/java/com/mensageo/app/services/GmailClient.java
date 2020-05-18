d2e2648f9d6398b4e8a286674e157e88905c9018
jcardenal <jcarde@thoughtworks.com>
2020-05-18 16:29:27 +0200

    Extract Gmail client creation to `@Configuration` bean and make integration test for sending emails executable inside a CI/CD pipeline
    Co-authored-by: Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>

--- src/main/java/com/mensageo/app/services/GmailClient.java
+++ src/main/java/com/mensageo/app/services/GmailClient.java
@@ -1,18 +1,7 @@
 package com.mensageo.app.services;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Base64;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.Message;
 import org.springframework.stereotype.Component;
 
@@ -20,27 +9,22 @@ import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.*;
-import java.nio.charset.Charset;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 
 @Component
 public class GmailClient implements MailerClient {
 
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
     private Gmail gmailService;
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String APPLICATION_NAME = "Mensageo Gmail API";
-    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_SEND);
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    public GmailClient(Gmail gmailService) {
+        this.gmailService = gmailService;
+    }
 
     @Override
-    public void sendEmail(EmailContent emailContent) throws MessagingException, IOException, GeneralSecurityException {
-        if (gmailService == null) authenticate();
+    public void sendEmail(EmailContent emailContent) throws MessagingException, IOException {
         sendMessage("mensageo.backend@gmail.com", createEmail(
                 "mensageo.backend@gmail.com",
                 "no-reply@gmail.com",
@@ -49,40 +33,12 @@ public class GmailClient implements MailerClient {
         ));
     }
 
-    private void authenticate() throws GeneralSecurityException, IOException {
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        gmailService = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
-        InputStream in = GmailClient.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
 
     private Message sendMessage(String userId,
                                 MimeMessage emailContent)
             throws IOException, javax.mail.MessagingException {
         Message message = createMessageWithEmail(emailContent);
-        message = gmailService.users().messages().send(userId, message).execute();
-
-        System.out.println("Message id: " + message.getId());
-        System.out.println(message.toPrettyString());
-        return message;
+        return gmailService.users().messages().send(userId, message).execute();
     }
 
     /**
--- src/test/java/com/mensageo/app/services/GmailClientTest.java
+++ src/test/java/com/mensageo/app/services/GmailClientTest.java
@@ -1,20 +1,44 @@
 package com.mensageo.app.services;
 
-import org.junit.Ignore;
+import com.google.api.services.gmail.Gmail;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GmailClientTest {
 
-    @Ignore
+    @MockBean
+    private Gmail gmailServiceMock;
+
+    @Mock
+    private Gmail.Users usersMock;
+
+    @Mock
+    private Gmail.Users.Messages messagesMock;
+
+    @Mock
+    private Gmail.Users.Messages.Send sendMock;
+
+    @Autowired
+    private GmailClient gmailClient;
+
     @Test
-    public void sendEmail() throws GeneralSecurityException, IOException, MessagingException {
-        // This test shouldn't run in the pipeline
-        // it just tests if emails can be sent via gmail
-        GmailClient gmailClient = new GmailClient();
+    public void sendEmail() throws IOException, MessagingException {
+
+        when(gmailServiceMock.users()).thenReturn(usersMock);
+        when(usersMock.messages()).thenReturn(messagesMock);
+        when(messagesMock.send(anyString(), any(com.google.api.services.gmail.model.Message.class))).thenReturn(sendMock);
 
         EmailContent emailContent = new EmailContent();
         emailContent.setCompany("Company name");
@@ -23,6 +47,10 @@ public class GmailClientTest {
 
         gmailClient.sendEmail(emailContent);
 
+        verify(messagesMock).send(eq("mensageo.backend@gmail.com"), any(com.google.api.services.gmail.model.Message.class));
 
+    }
+
+    private class Message {
     }
 }
