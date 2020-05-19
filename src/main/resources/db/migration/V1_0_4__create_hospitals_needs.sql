4c9c67669056c19145dcd5fd27e9dc204f7ad65d
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2020-05-19 16:16:38 +0200

    Add FK Constraint
    Co-authored-by: jcardenal <jcarde@thoughtworks.com>

--- src/main/resources/db/migration/V1_0_4__create_hospitals_needs.sql
+++ src/main/resources/db/migration/V1_0_4__create_hospitals_needs.sql
@@ -4,4 +4,5 @@ hospital_id bigint NOT NULL,
 product_id bigint NOT NULL,
 quantity bigint NOT NULL,
 request_timestamp timestamp NOT NULL default now(),
- FOREIGN KEY(hospital_id) REFERENCES hospital(id));
+ FOREIGN KEY(hospital_id) REFERENCES hospital(id),
+ FOREIGN KEY(product_id) REFERENCES product(id));
