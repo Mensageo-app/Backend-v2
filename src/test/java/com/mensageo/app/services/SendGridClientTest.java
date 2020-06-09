4e3cfd6f6f5ede6491ed44852fcae3ed9238e21b
jcardenal <jcarde@thoughtworks.com>
2020-06-09 15:56:55 +0200

    Make test more specific (again)

--- src/test/java/com/mensageo/app/services/SendGridClientTest.java
+++ src/test/java/com/mensageo/app/services/SendGridClientTest.java
@@ -1,11 +1,14 @@
 package com.mensageo.app.services;
 
+import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.hamcrest.MockitoHamcrest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,6 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -28,16 +34,36 @@ public class SendGridClientTest {
     @Autowired
     private SendGridClient sendGridClient;
 
+    private ArgumentCaptor<Request> argument = ArgumentCaptor.forClass(Request.class);
+
     @Test
     public void sendEmail() throws IOException {
 
         when(sendGridServiceMock.api(any(Request.class))).thenReturn(mockResponse);
+
         EmailContent emailContent = new EmailContent();
+
+        String mockPayload = "{\"from\":{\"email\":\"mensageo.backend@gmail.com\"}," +
+                "\"subject\":\"Someone has answered your request for product null\"," +
+                "\"personalizations\":[{\"to\":[{\"email\":\"mensageo.backend@gmail.com\"}]}]," +
+                "\"content\":[{\"type\":\"text/plain\",\"value\":\"Hi null,\\n\\nWe have good news! null has answered your " +
+                "request for product null, to provide 0 of it. Here is their information, please contact them.\\n\\nMaker " +
+                "and product information:\\n\\nFull Name: null\\nPhone: null\\nEmail: null\\nProduct Amount: 0\\n" +
+                "Description: null\\nOnce you have contacted the Maker and you have agreed a delivery date please inform " +
+                "us about it by replying to this email. In addition, we will be really grateful if once your order has been " +
+                "fulfilled you contact us again. In this way we guarantee that Mensageo.com will always have accurate and " +
+                "updated information and we can help more people.\\n\\nThank you for using Mensageo!! Please spread the " +
+                "word about it!\\n\\nRegards,\\nMensageo Team\"}]}";
 
         sendGridClient.sendEmail(emailContent);
 
-        verify(sendGridServiceMock).api(any(Request.class));
-
+        verify(sendGridServiceMock).api(MockitoHamcrest.argThat(
+                allOf(
+                        hasProperty("method", equalTo(Method.POST)),
+                        hasProperty("endpoint", equalTo("mail/send")),
+                        hasProperty("body", equalTo(mockPayload))
+                )
+        ));
     }
 
 
